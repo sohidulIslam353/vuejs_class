9b93fedb466037bd8605d09cefe5eefb384405bf
sohidulislam353 <sohidulislam353@gmail.com>
2019-06-18 12:45:24 -0700

    props

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h2>{{ title }}</h2>
+    <Navbar></Navbar>
+    <AllFriends :friends="friends"></AllFriends>
+    <OnlineFriends :friends="friends" />
+
+   
+  </div>
+  
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import AllFriends from './components/AllFriends.vue'
+import OnlineFriends from './components/OnlineFriends.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      title: 'Hurray ! Our vue apps are running :)',
+      friends:[
+                {name:'A', online:true},
+                {name:'B', online:true},
+                {name:'C', online:false},
+                {name:'D', online:true},
+                {name:'E', online:false}
+            ]
+    }
+  },
+  components:{
+    Navbar,
+    AllFriends,
+    OnlineFriends
+  }
+
+}
+</script>
+
+<style>
+h2 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/OnlineFriends.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="onlinefriends">
+      <h2>Online Friends</h2>
+       <div v-for="(friend,index) in friends" :key="index">
+           <h6 v-if="friend.online">{{friend.name}}</h6>
+       </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'OnlineFriends',
+    props:['friends'],
+    data(){
+        return{
+           
+        }
+    }
+}
+</script>
+
+<style scoped>
+h6 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color:green;
+}
+
+</style>
+
+
