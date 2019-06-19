0b028ac25ad2563b912ca3e6bcaca16c6b2c0ee6
sohidulislam353 <sohidulislam353@gmail.com>
2019-06-18 20:31:38 -0700

    filter

--- src/components/Hook.vue
+++ src/components/Hook.vue
@@ -1,41 +1,45 @@
 <template>
 <div class="Hook">
-    <h1>{{title}}</h1>
-    <button @click="changeTitle">click me</button>
+    <h1>Posts</h1><hr>
+    <div v-for="post in posts" :key="post.id">
+        <h2>{{post.title}}</h2>
+        <p>{{post.body | snippet}}</p><hr>
+    </div>
 </div>
 </template>
 
 <script>
+import axios from'axios'
 export default {
     name:'Hook',
     data(){
         return{
-            title:'Hook Part'
+           posts:[]
         }
     },
     methods:{
-        changeTitle(){
-            this.title = 'learn huter'
-        }
-    },
-    beforeCreate(){
-        alert('before acreated hook')
+      
     },
     created(){
-        alert('created hook')
-    },
-    beforeUpdate(){
-        alert('before Update')
-    },
-    updated(){
-        alert('update')
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response =>{
+            //console.log(response)
+            this.posts =response.data
+        })
+        .catch(error =>{
+            console.log(error)
+        })
     }
+   
 
 }
 </script>
 
 <style >
-
+h1{
+    color: red;
+    text-align: center;
+}
 
 </style>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// globally filter created
+Vue.filter('snippet' , val=>{
+  if(!val || typeof(val) != 'string') return ''
+  val = val.slice(0,40)
+  return val
+
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
